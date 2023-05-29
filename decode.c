feb0da7672de157c03512e46ac548b1ed79755f2
jon <basilieus@gmail.com>
2023-05-29 14:55:36 -0700

    Stopped at displacement

--- decode.c
+++ decode.c
@@ -1,3 +1,6 @@
+
+// stopped at displacement
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -12,11 +15,17 @@ typedef int8_t       int8;
 typedef int16_t      int16;
 
 #define MASK_W    0b001
-#define MASK_D    0b100
+#define MASK_D    0b010
+#define MASK_S    0b100
 
 #define MASK_MOD  0b011
 #define MASK_RM   0b111
 #define MASK_REG  0b111
+
+#define MODE_MEM0  0b00
+#define MODE_MEM8  0b01
+#define MODE_MEM16 0b10
+#define MODE_REG   0b11
 
 #define MOD(byte)  ((byte >> 6) & MASK_MOD)
 #define RM(byte)   ((byte >> 0) & MASK_RM)
@@ -51,9 +60,11 @@ typedef struct {
 } InstructionData;
 
 typedef struct {
-    InstructionData data;
-    uint            offset;
+    InstructionData structure;
+    uint16          data;
+    uint16          displacement;
     uint16          fields;
+    uint            offset;
 } Instruction;
 
 InstructionData instruction_table[0xC0] = {
@@ -89,9 +100,13 @@ const char *get_instruction_name(TYPE type) {
 };
 
 
-int parse_instruction(Instruction *instruction, const uint8 *data, uint size, uint offset) {
+int parse_instruction(Instruction *instruction,  uint8 * const data, uint size, uint offset) {
     InstructionData instruction_data;
     uint8 *raw = data + offset;
+    uint8 lo = 0, hi = 0;
+    uint8 mod, rm;
+    uint data_size = 1, disp_size = 0;
+
     memset(instruction, 0, sizeof(*instruction));
 
     instruction_data = instruction_table[raw[0]];
@@ -103,23 +118,72 @@ int parse_instruction(Instruction *instruction, const uint8 *data, uint size, ui
 
     // set fields for [mod ... r/m]
     switch (instruction_data.format) {
+        case RM_IMM:
         case RM_REG:
-            instruction->fields |= (MOD(raw[1]) & MASK_MOD) << 0;
-            instruction->fields |= (RM(raw[1])  & MASK_RM)  << 4;
+            mod = MOD(raw[1]);
+            rm  = RM(raw[1]);
+
+            // direct address and 16-bit displacement
+            if (mod == MODE_MEM16 || (mod == MODE_MEM0 && rm == 0b110))
+                disp_size = 2;
+
+            // 8-bit displacement
+            if (mod == MODE_MEM8)
+                disp_size = 1;
+
+            instruction_data.size += disp_size;
+            instruction->displacement = (raw[3] << 8) * (disp_size > 1) | raw[2];
+            instruction->fields |= (mod & MASK_MOD) << 0;
+            instruction->fields |= (rm  & MASK_RM)  << 4;
+            break;
         default:
             break;
     };
 
     // set fields for reg
     switch (instruction_data.format) {
+        case REG_IMM:
+            instruction->fields |= (REG2(raw[0]) & MASK_REG) << 7;
+            break;
         case RM_REG:
             instruction->fields |= (REG(raw[1]) & MASK_REG) << 7;
+            break;
         default:
             break;
     };
 
+
+    // for data/addr fields
+    switch (instruction_data.format) {
+        case REG_IMM:
+            if (instruction_data.flags & MASK_S) {
+                instruction->data = raw[instruction_data.size - data_size];
+                if (instruction->data & 0x80)
+                    instruction->data |= 0xFF00;
+                break;
+            }
+
+            if (W(instruction_data.flags)) {
+                data_size = 2;
+                hi = raw[instruction_data.size - data_size + 1];
+            }
+
+            lo = raw[instruction_data.size - data_size];
+            instruction->data = (hi << 8) | lo;
+            break;
+        default:
+            break;
+    }
+
+    if (offset + instruction_data.size > size) {
+        fprintf(stderr, "out of image boundaries (offset: %u, "
+                "inst_size: %u, image_size: %u)\n", offset, instruction_data.size,
+                size);
+        return -2;
+    }
+
     instruction->offset = offset;
-    instruction->data = instruction_data;
+    instruction->structure = instruction_data;
     return 0;
 };
 
@@ -135,7 +199,13 @@ int scan_instructions(Instruction *const instructions, uint count, uint8 *const 
             if (parse_instruction(&instruction, data, size, offset) < 0) {
                 return -1;
             }
-            offset += instruction.data.size;
+            
+            if (instruction.structure.type == UNKNOWN) {
+				fprintf(stderr, "unknown instruction 0x%02X at interation %d\n", data[offset], count);
+				return -2;
+			}
+
+            offset += instruction.structure.size;
         }
         return count;
     }
@@ -144,15 +214,16 @@ int scan_instructions(Instruction *const instructions, uint count, uint8 *const 
         if (parse_instruction(instructions + i, data, size, offset) < 0) {
             return -1;
         }
-        offset += instructions[i].data.size;
+        offset += instructions[i].structure.size;
     }
     return 0;
 }; 
 
 void decode_instruction(FILE *out, Instruction *instruction) {
+    int16 disp = *((int16 *)&instruction->displacement);
     uint8 w;
-    w = W(instruction->data.flags);
-    fprintf(out, "%s ",  get_instruction_name(instruction->data.type));    
+    w = W(instruction->structure.flags);
+    fprintf(out, "%s ",  get_instruction_name(instruction->structure.type));    
     fprintf(out, "%s, ", regs[w][FIELD_RM(instruction->fields)]); 
     fprintf(out, "%s\n", regs[w][FIELD_REG(instruction->fields)]); 
 };
@@ -192,7 +263,7 @@ int main(int argc, char **argv) {
     uint offset;
 
     fprintf(stdout, "bits 16\n\n");
-    for (i=0, offset=0; i < instruction_count && offset < size; ++i, offset += instructions[i].data.size) {
+    for (i=0, offset=0; i < instruction_count && offset < size; ++i, offset += instructions[i].structure.size) {
         decode_instruction(stdout, instructions + i);
     }
 
