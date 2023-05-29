62bf2e71fda04c1d4380675d06f23c2773609144
jon <basilieus@gmail.com>
2023-05-29 10:19:50 -0700

    Working

--- disasm.c
+++ disasm.c
@@ -10,71 +10,75 @@ typedef uint32_t     uint32;
 typedef int8_t       int8;
 typedef int16_t      int16;
 
-#define MASK_W  0b01
-#define MASK_D  0b10
+#define MASK_W    0b001
+#define MASK_D    0b100
 
-#define MOD(byte)  ((byte >> 6) & 0b011)
-#define RM(byte)   ((byte >> 0) & 0b111)
-#define REG(byte)  ((byte >> 3) & 0b111)
-#define REG2(byte) ((byte >> 0) & 0b111)
+#define MASK_MOD  0b011
+#define MASK_RM   0b111
+#define MASK_REG  0b111
 
-const char *regs[2][8] = {
+#define MOD(byte)  ((byte >> 6) & MASK_MOD)
+#define RM(byte)   ((byte >> 0) & MASK_RM)
+#define REG(byte)  ((byte >> 3) & MASK_REG)
+#define REG2(byte) ((byte >> 0) & MASK_REG)
+
+#define FIELD_RM(fields)  ((fields >> 4) & MASK_RM)
+#define FIELD_REG(fields) ((fields >> 7) & MASK_REG)
+
+#define W(flags) (!!(flags & MASK_W))
+
+static const char *regs[2][8] = {
     { "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh" },
     { "ax", "cx", "dx", "bx", "sp", "bp", "si", "di" }
 };
 
-enum inst_type {
-    UNK = 0,
-    MOV, MOVSB, MOVSW,
-    EXTD,
+typedef enum {
+    UNKNOWN,
+    MOV, 
+} TYPE;
+
+typedef enum {
+    RM_REG,
+    REG_IMM,
+} FORMAT;
+
+typedef struct {
+    TYPE   type;
+    FORMAT format;
+    uint8  flags;
+    uint8  size;
+} InstructionData;
+
+typedef struct {
+    InstructionData data;
+    uint16          fields;
+} Instruction;
+
+InstructionData instruction_table[0xC0] = {
+          // TYPE, FORMAT     ,  FLAGS,       SIZE 
+    [0x88]= { MOV, RM_REG,  0,              2 }, // 0x88
+            { MOV, RM_REG,  MASK_W,         2 }, // 0x89
+            { MOV, RM_REG,  MASK_D,         2 }, // 0x8A
+            { MOV, RM_REG,  MASK_D|MASK_W,  2 }, // 0x8B
+    [0xB0]= { MOV, REG_IMM, 0,              2 }, // 0xB0
+            { MOV, REG_IMM, 0,              2 }, // 0xB1
+            { MOV, REG_IMM, 0,              2 }, // 0xB2
+            { MOV, REG_IMM, 0,              2 }, // 0xB3
+            { MOV, REG_IMM, 0,              2 }, // 0xB4
+            { MOV, REG_IMM, 0,              2 }, // 0xB5
+            { MOV, REG_IMM, 0,              2 }, // 0xB6
+            { MOV, REG_IMM, 0,              2 }, // 0xB7
+            { MOV, REG_IMM, MASK_W,         3 }, // 0xB8
+            { MOV, REG_IMM, MASK_W,         3 }, // 0xB9
+            { MOV, REG_IMM, MASK_W,         3 }, // 0xBA
+            { MOV, REG_IMM, MASK_W,         3 }, // 0xBB
+            { MOV, REG_IMM, MASK_W,         3 }, // 0xBC
+            { MOV, REG_IMM, MASK_W,         3 }, // 0xBD
+            { MOV, REG_IMM, MASK_W,         3 }, // 0xBE
+            { MOV, REG_IMM, MASK_W,         3 }, // 0xBF
 };
 
-enum inst_format {
-    FMT_RM_REG,
-    FMT_REG_IMM,
-};
-
-struct inst_data {
-    enum inst_type   type;
-    enum inst_format fmt;
-         uint8       flags;
-         uint8       prefixes;
-         uint8       size;
-};
-
-struct inst {
-    struct inst_data    base;
-           uint16       disp;
-           uint16       data;     // addr, imm
-           uint16       data_ext; // if instruction size is 6 bytes
-           uint16       fields;   // mod, reg, r/m, sr, esc
-           uint         offset;   // location in image
-};
-
-struct inst_data inst_table[256] = {
-    [0x88]= { MOV, FMT_RM_REG,  0,       0, 2 }, // 0x88
-            { MOV, FMT_RM_REG,  MASK_W,     0, 2 }, // 0x89
-            { MOV, FMT_RM_REG,  MASK_D,     0, 2 }, // 0x8A
-            { MOV, FMT_RM_REG,  MASK_D|MASK_W, 0, 2 }, // 0x8B
-    [0xB0]= { MOV, FMT_REG_IMM, 0,       0, 2 }, // 0xB0
-            { MOV, FMT_REG_IMM, 0,       0, 2 }, // 0xB1
-            { MOV, FMT_REG_IMM, 0,       0, 2 }, // 0xB2
-            { MOV, FMT_REG_IMM, 0,       0, 2 }, // 0xB3
-            { MOV, FMT_REG_IMM, 0,       0, 2 }, // 0xB4
-            { MOV, FMT_REG_IMM, 0,       0, 2 }, // 0xB5
-            { MOV, FMT_REG_IMM, 0,       0, 2 }, // 0xB6
-            { MOV, FMT_REG_IMM, 0,       0, 2 }, // 0xB7
-            { MOV, FMT_REG_IMM, MASK_W,     0, 3 }, // 0xB8
-            { MOV, FMT_REG_IMM, MASK_W,     0, 3 }, // 0xB9
-            { MOV, FMT_REG_IMM, MASK_W,     0, 3 }, // 0xBA
-            { MOV, FMT_REG_IMM, MASK_W,     0, 3 }, // 0xBB
-            { MOV, FMT_REG_IMM, MASK_W,     0, 3 }, // 0xBC
-            { MOV, FMT_REG_IMM, MASK_W,     0, 3 }, // 0xBD
-            { MOV, FMT_REG_IMM, MASK_W,     0, 3 }, // 0xBE
-            { MOV, FMT_REG_IMM, MASK_W,     0, 3 }, // 0xBF
-};
-
-const char *get_instruction_name(enum inst_type type) {
+const char *get_instruction_name(TYPE type) {
     switch (type) {
         case MOV: return "mov";
         default:
@@ -84,7 +88,7 @@ const char *get_instruction_name(enum inst_type type) {
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        printf("Missing file to disasm\n");
+        printf("Missing file to decode. Usage: decode <filename>\n");
         return 0;
     }
     
@@ -95,31 +99,43 @@ int main(int argc, char **argv) {
     size = ftell(f);
     assert(fseek(f, 0, SEEK_SET) == 0);
 
-    uint8_t *data = malloc(size);
-    if (data == NULL) exit(127);
-    assert(fread(data, size, 1, f) == 1);
+    uint8_t *raw_data = malloc(size);
+    if (raw_data == NULL) exit(127);
+    assert(fread(raw_data, size, 1, f) == 1);
 
     fclose(f);
 
-    struct inst_data mov = inst_table[data[0]];
+    InstructionData instruction_data = instruction_table[raw_data[0]];
 
-    printf("bits 16\n\n");
-    printf("%s", get_instruction_name(mov.type));    
-    switch (mov.type) {
-        case MOV:
-            if (mov.fmt == FMT_RM_REG) {
-                uint8 mod, rm, reg;
-                mod = MOD(data[1]);
-                rm  = RM(data[1]); 
-                reg = REG(data[1]);
+    Instruction instruction = {0};
 
-            }
-            break;
+    // set fields for [mod ... r/m]
+    switch (instruction_data.format) {
+        case RM_REG:
+            instruction.fields |= (MOD(raw_data[1]) & MASK_MOD) << 0;
+            instruction.fields |= (RM(raw_data[1])  & MASK_RM)  << 4;
         default:
-            printf("instruction not found\n");
+            break;
     };
 
-    free(data);
-    return 0;
+    // set fields for reg
+    switch (instruction_data.format) {
+        case RM_REG:
+            instruction.fields |= (REG(raw_data[1]) & MASK_REG) << 7;
+        default:
+            break;
+    };
 
+    instruction.data = instruction_data;
+
+    uint8 w;
+    w = W(instruction_data.flags);
+    printf("bits 16\n\n");
+    printf("%s ",  get_instruction_name(instruction.data.type));    
+    printf("%s, ", regs[w][FIELD_RM(instruction.fields)]); 
+    printf("%s\n", regs[w][FIELD_REG(instruction.fields)]); 
+
+
+    free(raw_data);
+    return 0;
 }
